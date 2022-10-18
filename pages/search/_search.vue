4052898b33c152a927e9a3228d56e81c5c55d02e
H-H-Liao <liao@wanliroad.com>
2022-10-18 23:55:04 +0800

    feat:增加搜尋空白頁面

---
+++ pages/search/_search.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container not-banner">
+        搜尋頁面
+        <!-- 標籤 -->
+        <!-- 內容 -->
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
