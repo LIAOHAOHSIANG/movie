c932b69aeba8eac6774235e17cc36780917ac174
H-H-Liao <liao@wanliroad.com>
2022-10-18 23:42:04 +0800

    feat:增加RWD斷點與meta

---
+++ nuxt.config.js
@@ -0,0 +1,8 @@
+export default {
+    head: {
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        ]
+    }
+}
